ae4cd2754d233eb609e89fc813d66db76d8c6fe9
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-26 11:41:15 +0100

    Added Spectre.Console table for difficulty selection

---
+++ Blackout/View.cs
@@ -0,0 +1,20 @@
+using System;
+using Spectre.Console;
+
+namespace Blackout
+{
+    public class View
+    {
+        private static void Main(string[] args)
+        {
+            var table = new Table();
+                table.AddColumn("Difficulty");
+                table.AddColumn("Size");
+                table.AddRow("[green]Easy[/]", "3 x 3");
+                table.AddRow("[yellow]Medium[/]", "5 x 5");
+                table.AddRow("[red]Hard[/]", "8 x 8");
+                table.AddRow("Custom[/]", "? x ?");
+            AnsiConsole.Write(table);
+        }
+    }
+}
