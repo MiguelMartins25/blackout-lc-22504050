360c571231214a8ecf9084f5ae47bda85ba0f236
Miguel Martins <miguelarbmartins@gmail.com>
2026-05-27 16:25:26 +0100

    Inicio de Documentação em README.md e adicionado documentação XML em controller.cs

--- Blackout/Controller.cs
+++ Blackout/Controller.cs
@@ -8,9 +8,24 @@ namespace Blackout
 {
     public class Controller
     {
+        /// <summary>
+        /// Determina as dimensões da grelha com base
+        /// na dificuldade selecionada pelo utilizador.
+        /// </summary>
+        /// <param name="choice">
+        /// Dificuldade escolhida pelo utilizador.
+        /// </param>
+        /// <param name="view">
+        /// Instância responsável pela interação com o utilizador.
+        /// </param>
+        /// <returns>
+        /// Um tuplo contendo o número de linhas e colunas da grelha.
+        /// </returns>
+        /// <remarks>
         /// Foi usado AI para dar debug e concertar um erro com looping de inputs
         /// A lógica e concepção foi feita por nós, apenas foi usado Inteligência
         /// Artificial para evitar o erro.
+        /// </remarks>
         public (int, int) GridBuilder(string choice, View view)
         {
             return choice switch
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Projeto-Final-LP1
-blah blah blah temos até 27 de maio à meia-noite
+# Projeto-Final-LP1: Blackout
+
+### Grupo de projeto:
+#### Samuel Esteves - a22407214
+-
+#### Miguel Martins - a22504050
+-
+
+---
+### Repositório Git: https://github.com/whamslambamsam/Projeto-Final-LP1
+---
+### Arquitetura da Solução e Algoritmos utilizados
+
+---
+### Diagrama UML de Classes
+
+---
+### Referências e bibliotecas utilizadas
